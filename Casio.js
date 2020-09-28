2452e67ceffc8e7dce26e6afc10239b98ddf02a6
Aditya <65461420+Adi142857@users.noreply.github.com>
2020-09-28 16:07:31 +0530

    Add files via upload

---
+++ Casio.js
@@ -0,0 +1,8 @@
+window.onload = function(){              
+    loading = document.getElementById("square");
+};           
+setTimeout(function(){
+    square.style.display = 'none';
+}, 4000);
+alert("Please listen these songs.I played it in my casio 😇")
+alert("Also keep volume full..😃")
